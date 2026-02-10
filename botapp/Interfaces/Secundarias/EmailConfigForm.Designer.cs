7b44180772091091e428f2aa41064dfd36c01558
WCRUZ20 <wcruz@est.ecotec.edu.ec>
2026-02-09 23:16:07 -0500

    mejora interface configuracion correo

--- botapp/Interfaces/Secundarias/EmailConfigForm.Designer.cs
+++ botapp/Interfaces/Secundarias/EmailConfigForm.Designer.cs
@@ -6,6 +6,25 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TableLayoutPanel layout;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.CheckBox chkSsl;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnProbar;
+        private System.Windows.Forms.FlowLayoutPanel panelBotones;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Label lblPuerto;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblSeguridad;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,10 +47,311 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.layout = new System.Windows.Forms.TableLayoutPanel();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.lblPuerto = new System.Windows.Forms.Label();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.lblSeguridad = new System.Windows.Forms.Label();
+            this.chkSsl = new System.Windows.Forms.CheckBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.panelBotones = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnProbar = new System.Windows.Forms.Button();
+            this.layout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.panelBotones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // layout
+            // 
+            this.layout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(47)))));
+            this.layout.ColumnCount = 2;
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.74138F));
+            this.layout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.25862F));
+            this.layout.Controls.Add(this.lblHost, 0, 0);
+            this.layout.Controls.Add(this.txtHost, 1, 0);
+            this.layout.Controls.Add(this.lblPuerto, 0, 1);
+            this.layout.Controls.Add(this.numPort, 1, 1);
+            this.layout.Controls.Add(this.lblUsuario, 0, 2);
+            this.layout.Controls.Add(this.txtUsuario, 1, 2);
+            this.layout.Controls.Add(this.lblPassword, 0, 3);
+            this.layout.Controls.Add(this.txtPassword, 1, 3);
+            this.layout.Controls.Add(this.lblFrom, 0, 4);
+            this.layout.Controls.Add(this.txtFrom, 1, 4);
+            this.layout.Controls.Add(this.lblTo, 0, 5);
+            this.layout.Controls.Add(this.txtTo, 1, 5);
+            this.layout.Controls.Add(this.lblSeguridad, 0, 6);
+            this.layout.Controls.Add(this.chkSsl, 1, 6);
+            this.layout.Controls.Add(this.lblEstado, 0, 7);
+            this.layout.Controls.Add(this.panelBotones, 0, 8);
+            this.layout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layout.Location = new System.Drawing.Point(0, 0);
+            this.layout.Margin = new System.Windows.Forms.Padding(0);
+            this.layout.Name = "layout";
+            this.layout.Padding = new System.Windows.Forms.Padding(16);
+            this.layout.RowCount = 9;
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
+            this.layout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.layout.Size = new System.Drawing.Size(463, 405);
+            this.layout.TabIndex = 0;
+            // 
+            // lblHost
+            // 
+            this.lblHost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblHost.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHost.ForeColor = System.Drawing.Color.White;
+            this.lblHost.Location = new System.Drawing.Point(19, 16);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(122, 42);
+            this.lblHost.TabIndex = 0;
+            this.lblHost.Text = "Host SMTP";
+            this.lblHost.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtHost
+            // 
+            this.txtHost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtHost.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHost.Location = new System.Drawing.Point(147, 19);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(297, 25);
+            this.txtHost.TabIndex = 1;
+            // 
+            // lblPuerto
+            // 
+            this.lblPuerto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPuerto.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuerto.ForeColor = System.Drawing.Color.White;
+            this.lblPuerto.Location = new System.Drawing.Point(19, 58);
+            this.lblPuerto.Name = "lblPuerto";
+            this.lblPuerto.Size = new System.Drawing.Size(122, 42);
+            this.lblPuerto.TabIndex = 2;
+            this.lblPuerto.Text = "Puerto";
+            this.lblPuerto.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // numPort
+            // 
+            this.numPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numPort.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.numPort.Location = new System.Drawing.Point(147, 61);
+            this.numPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numPort.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(297, 25);
+            this.numPort.TabIndex = 3;
+            this.numPort.Value = new decimal(new int[] {
+            587,
+            0,
+            0,
+            0});
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblUsuario.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.Color.White;
+            this.lblUsuario.Location = new System.Drawing.Point(19, 100);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(122, 42);
+            this.lblUsuario.TabIndex = 4;
+            this.lblUsuario.Text = "Usuario";
+            this.lblUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtUsuario.Location = new System.Drawing.Point(147, 103);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(297, 25);
+            this.txtUsuario.TabIndex = 5;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.White;
+            this.lblPassword.Location = new System.Drawing.Point(19, 142);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(122, 42);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Contraseña";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtPassword.Location = new System.Drawing.Point(147, 145);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.Size = new System.Drawing.Size(297, 25);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblFrom.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrom.ForeColor = System.Drawing.Color.White;
+            this.lblFrom.Location = new System.Drawing.Point(19, 184);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(122, 42);
+            this.lblFrom.TabIndex = 8;
+            this.lblFrom.Text = "Correo remitente";
+            this.lblFrom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFrom.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtFrom.Location = new System.Drawing.Point(147, 187);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(297, 25);
+            this.txtFrom.TabIndex = 9;
+            // 
+            // lblTo
+            // 
+            this.lblTo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTo.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTo.ForeColor = System.Drawing.Color.White;
+            this.lblTo.Location = new System.Drawing.Point(19, 226);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(122, 42);
+            this.lblTo.TabIndex = 10;
+            this.lblTo.Text = "Correo destino";
+            this.lblTo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtTo.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtTo.Location = new System.Drawing.Point(147, 229);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(297, 25);
+            this.txtTo.TabIndex = 11;
+            // 
+            // lblSeguridad
+            // 
+            this.lblSeguridad.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblSeguridad.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeguridad.ForeColor = System.Drawing.Color.White;
+            this.lblSeguridad.Location = new System.Drawing.Point(19, 268);
+            this.lblSeguridad.Name = "lblSeguridad";
+            this.lblSeguridad.Size = new System.Drawing.Size(122, 42);
+            this.lblSeguridad.TabIndex = 12;
+            this.lblSeguridad.Text = "Seguridad";
+            this.lblSeguridad.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // chkSsl
+            // 
+            this.chkSsl.AutoSize = true;
+            this.chkSsl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.chkSsl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkSsl.ForeColor = System.Drawing.Color.White;
+            this.chkSsl.Location = new System.Drawing.Point(147, 271);
+            this.chkSsl.Name = "chkSsl";
+            this.chkSsl.Size = new System.Drawing.Size(83, 36);
+            this.chkSsl.TabIndex = 13;
+            this.chkSsl.Text = "Usar SSL";
+            this.chkSsl.UseVisualStyleBackColor = true;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.layout.SetColumnSpan(this.lblEstado, 2);
+            this.lblEstado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblEstado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(65)))), ((int)(((byte)(81)))));
+            this.lblEstado.Location = new System.Drawing.Point(19, 310);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(425, 48);
+            this.lblEstado.TabIndex = 14;
+            this.lblEstado.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panelBotones
+            // 
+            this.layout.SetColumnSpan(this.panelBotones, 2);
+            this.panelBotones.Controls.Add(this.btnGuardar);
+            this.panelBotones.Controls.Add(this.btnProbar);
+            this.panelBotones.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBotones.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.panelBotones.Location = new System.Drawing.Point(19, 361);
+            this.panelBotones.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.panelBotones.Name = "panelBotones";
+            this.panelBotones.Size = new System.Drawing.Size(425, 41);
+            this.panelBotones.TabIndex = 15;
+            this.panelBotones.WrapContents = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(197)))), ((int)(((byte)(94)))));
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.Color.White;
+            this.btnGuardar.Location = new System.Drawing.Point(302, 3);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(120, 32);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnProbar
+            // 
+            this.btnProbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(99)))), ((int)(((byte)(235)))));
+            this.btnProbar.FlatAppearance.BorderSize = 0;
+            this.btnProbar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProbar.ForeColor = System.Drawing.Color.White;
+            this.btnProbar.Location = new System.Drawing.Point(156, 3);
+            this.btnProbar.Name = "btnProbar";
+            this.btnProbar.Size = new System.Drawing.Size(140, 32);
+            this.btnProbar.TabIndex = 1;
+            this.btnProbar.Text = "Enviar prueba";
+            this.btnProbar.UseVisualStyleBackColor = false;
+            this.btnProbar.Click += new System.EventHandler(this.btnProbar_Click);
+            // 
+            // EmailConfigForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "EmailConfigForm";
+            this.ClientSize = new System.Drawing.Size(463, 405);
+            this.Controls.Add(this.layout);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EmailConfigForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Configuración de Correo";
+            this.layout.ResumeLayout(false);
+            this.layout.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.panelBotones.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- botapp/Interfaces/Secundarias/EmailConfigForm.cs
+++ botapp/Interfaces/Secundarias/EmailConfigForm.cs
@@ -14,137 +14,11 @@ namespace botapp.Interfaces.Secundarias
 {
     internal partial class EmailConfigForm : Form
     {
-        private TextBox txtHost;
-        private NumericUpDown numPort;
-        private TextBox txtUsuario;
-        private TextBox txtPassword;
-        private TextBox txtFrom;
-        private TextBox txtTo;
-        private CheckBox chkSsl;
-        private Label lblEstado;
-        private Button btnGuardar;
-        private Button btnProbar;
-
+        
         public EmailConfigForm()
         {
             InitializeComponent();
-            InitializeComponent2();
             CargarConfiguracion();
-        }
-
-        private void InitializeComponent2()
-        {
-            Text = "Configuración de Correo";
-            StartPosition = FormStartPosition.CenterParent;
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-            MaximizeBox = false;
-            MinimizeBox = false;
-            ClientSize = new Size(560, 390);
-
-            var layout = new TableLayoutPanel
-            {
-                Dock = DockStyle.Fill,
-                ColumnCount = 2,
-                Padding = new Padding(16),
-            };
-            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35));
-            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 65));
-
-            txtHost = CrearTextBox();
-            numPort = new NumericUpDown { Dock = DockStyle.Fill, Minimum = 1, Maximum = 65535, Value = 587 };
-            txtUsuario = CrearTextBox();
-            txtPassword = CrearTextBox();
-            txtPassword.PasswordChar = '●';
-            txtFrom = CrearTextBox();
-            txtTo = CrearTextBox();
-            chkSsl = new CheckBox { Text = "Usar SSL", Dock = DockStyle.Left, AutoSize = true };
-
-            lblEstado = new Label
-            {
-                Dock = DockStyle.Fill,
-                AutoSize = true,
-                ForeColor = Color.FromArgb(55, 65, 81),
-                TextAlign = ContentAlignment.MiddleLeft
-            };
-
-            btnGuardar = new Button
-            {
-                Text = "Guardar",
-                Dock = DockStyle.Right,
-                Width = 120,
-                BackColor = Color.FromArgb(34, 197, 94),
-                ForeColor = Color.White,
-                FlatStyle = FlatStyle.Flat
-            };
-            btnGuardar.FlatAppearance.BorderSize = 0;
-            btnGuardar.Click += btnGuardar_Click;
-
-            btnProbar = new Button
-            {
-                Text = "Enviar prueba",
-                Dock = DockStyle.Right,
-                Width = 140,
-                BackColor = Color.FromArgb(37, 99, 235),
-                ForeColor = Color.White,
-                FlatStyle = FlatStyle.Flat
-            };
-            btnProbar.FlatAppearance.BorderSize = 0;
-            btnProbar.Click += btnProbar_Click;
-
-            AgregarFila(layout, "Host SMTP", txtHost);
-            AgregarFila(layout, "Puerto", numPort);
-            AgregarFila(layout, "Usuario", txtUsuario);
-            AgregarFila(layout, "Contraseña", txtPassword);
-            AgregarFila(layout, "Correo remitente", txtFrom);
-            AgregarFila(layout, "Correo destino", txtTo);
-            AgregarFila(layout, "Seguridad", chkSsl);
-
-            layout.RowStyles.Add(new RowStyle(SizeType.Absolute, 48));
-            layout.Controls.Add(lblEstado, 0, 7);
-            layout.SetColumnSpan(lblEstado, 2);
-
-            var panelBotones = new FlowLayoutPanel
-            {
-                Dock = DockStyle.Fill,
-                FlowDirection = FlowDirection.RightToLeft,
-                WrapContents = false,
-                Padding = new Padding(0),
-                Margin = new Padding(0)
-            };
-            panelBotones.Controls.Add(btnGuardar);
-            panelBotones.Controls.Add(btnProbar);
-
-            layout.RowStyles.Add(new RowStyle(SizeType.Absolute, 44));
-            layout.Controls.Add(panelBotones, 0, 8);
-            layout.SetColumnSpan(panelBotones, 2);
-
-            Controls.Add(layout);
-        }
-
-        private static TextBox CrearTextBox()
-        {
-            return new TextBox
-            {
-                Dock = DockStyle.Fill,
-                Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)))
-            };
-        }
-
-        private static void AgregarFila(TableLayoutPanel layout, string etiqueta, Control control)
-        {
-            int fila = layout.RowCount++;
-            layout.RowStyles.Add(new RowStyle(SizeType.Absolute, 42));
-
-            var label = new Label
-            {
-                Text = etiqueta,
-                Dock = DockStyle.Fill,
-                TextAlign = ContentAlignment.MiddleLeft,
-                Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)))
-            };
-
-            layout.Controls.Add(label, 0, fila);
-            layout.Controls.Add(control, 1, fila);
         }
 
         private void CargarConfiguracion()
@@ -173,14 +47,6 @@ namespace botapp.Interfaces.Secundarias
         private void GuardarConfiguracion()
         {
             var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-
-            UpdateAppSetting(config, "BOT_SMTP_HOST", txtHost.Text.Trim());
-            UpdateAppSetting(config, "BOT_SMTP_PORT", numPort.Value.ToString());
-            UpdateAppSetting(config, "BOT_SMTP_USER", txtUsuario.Text.Trim());
-            UpdateAppSetting(config, "BOT_SMTP_PASSWORD", txtPassword.Text);
-            UpdateAppSetting(config, "BOT_SMTP_FROM", txtFrom.Text.Trim());
-            UpdateAppSetting(config, "BOT_REPORT_EMAIL_TO", txtTo.Text.Trim());
-            UpdateAppSetting(config, "BOT_SMTP_ENABLE_SSL", chkSsl.Checked ? "true" : "false");
 
             UpdateAppSetting(config, "SmtpHost", txtHost.Text.Trim());
             UpdateAppSetting(config, "SmtpPort", numPort.Value.ToString());
